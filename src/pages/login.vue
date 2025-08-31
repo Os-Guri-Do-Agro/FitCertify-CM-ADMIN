118c6a39c67a3bcf08454549b5e5bc276351f760
Luis Gustavo <darkbj13.lg@gmail.com>
2025-08-31 18:31:12 -0300

    fix: ajusta design e interatividade em login

--- src/pages/login.vue
+++ src/pages/login.vue
@@ -1,33 +1,76 @@
 <template>
-  <v-container fluid class="fill-height pa-0 d-flex align-center justify-center background-image">
+  <v-container
+    fluid
+    class="fill-height pa-0 d-flex align-center justify-center background-image"
+  >
     <v-row class="justify-center align-center w-100 h-100 ma-0">
       <v-col cols="12" sm="8" md="4">
-        <v-card elevation="10" rounded="xl" class="pa-6">
+        <v-card elevation="8" min-height="600px" rounded="xl" class="px-2 px-md-5 px-md-10 py-6 blur-bg d-flex flex-column justify-center">
           <v-card-title class="text-center d-flex flex-column align-center">
-            <v-img src="/src/assets/Camada_1.png" alt="Logo" width="300" class="mb-3" contain></v-img>
-            <div class="text-h6 font-weight-medium text-grey-darken-2">
-              Bem-vindo de volta
+            <v-img
+              src="/src/assets/FITCertify365_Logo1.png"
+              alt="Logo"
+              width="300"
+              contain
+            ></v-img>
+            <div
+              class="text-h6 font-weight-medium text-grey-darken-2 w-100 text-center mt-10"
+            >
+              <span class="text-md-h5 font-weight-bold text-white">Bem-vindo de volta!</span>
             </div>
           </v-card-title>
 
-          <v-card-text class="pt-0">
+          <v-card-text class="pt-0 mt-5">
             <v-form>
-              <v-text-field label="Email" v-model="data.email" prepend-inner-icon="mdi-email" variant="outlined"
-                density="comfortable" class="mb-4"></v-text-field>
+              <label class="text-white text-subtitle-1" for="inputEmail"
+                >E-mail</label
+              >
+              <v-text-field
+                v-model="data.email"
+                prepend-inner-icon="mdi-email"
+                variant="solo"
+                bg-color="white"
+                name="inputEmail"
+                placeholder="E-mail"
+                density="comfortable"
+                class="text-white mt-2"
+                rounded="lg"
+              ></v-text-field>
 
-              <v-text-field label="Senha" v-model="data.senha" prepend-inner-icon="mdi-lock" type="password"
-                variant="outlined" density="comfortable" class="mb-4"></v-text-field>
-
-              <v-btn color="primary" size="large" block rounded class="mb-3" @click="login" :loading="loading">
-                Entrar
-              </v-btn>
-
-              <div class="text-center">
-                <a href="#" class="text-primary text-decoration-none">
+              <label class="text-white text-subtitle-1" for="inputSenha"
+                >Senha</label
+              >
+              <v-text-field
+                v-model="data.senha"
+                prepend-inner-icon="mdi-lock"
+                :type="showPassword ? 'text' : 'password'"
+                :append-inner-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
+                @click:append-inner="showPassword = !showPassword"
+                placeholder="Senha"
+                variant="solo"
+                name="inputSenha"
+                bg-color="white"
+                density="comfortable"
+                class="text-white mt-2"
+                rounded="lg"
+              ></v-text-field>
+              <div class="text-start text-subtitle-1">
+                <a href="#" class="text-white text-decoration-none">
                   Esqueceu a senha?
                 </a>
-               
               </div>
+
+              <v-btn
+                size="large"
+                block
+                rounded="lg"
+                height="50px"
+                class="mt-5 color-btn"
+                @click="login"
+                :loading="loading"
+              >
+                Entrar
+              </v-btn>
             </v-form>
           </v-card-text>
         </v-card>
@@ -50,13 +93,14 @@ const data = ref({
 })
 
 const loading = ref(false)
+const showPassword = ref(false)
 
 const login = async () => {
   loading.value = true
   try {
     const response = await authService.login(data.value)
     if (response && response.access_token) {
-      console.log('Login realizado com sucesso')
+      toast.success('Login realizado com sucesso!', { autoClose: 3000 })
       sessionStorage.setItem('token', response.access_token)
 
       const payload = JSON.parse(atob(response.access_token.split('.')[1]))
@@ -73,6 +117,8 @@ const login = async () => {
 
       router.push('/')
     }
+  } catch (error) {
+    toast.error('Erro ao fazer login. Verifique suas credenciais.')
   } finally {
     loading.value = false
   }
@@ -87,5 +133,15 @@ const removeToken = () => {
 .background-image {
   background: url('/src/assets/image-teste-5.png') no-repeat center center;
   background-size: cover;
+}
+
+.blur-bg {
+  backdrop-filter: blur(10px);
+  background-color: transparent;
+}
+
+.color-btn {
+  background-color: #00C6FE;
+  color: white;
 }
 </style>
