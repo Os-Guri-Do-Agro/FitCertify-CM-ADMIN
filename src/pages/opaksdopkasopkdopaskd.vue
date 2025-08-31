f9fed66a60ff0a45549e5bd21eac5b29c6010030
AW5inspirion <nicolas@aw5.com.br>
2025-08-31 15:24:57 -0400

    feat: insert a new tables

--- src/pages/opaksdopkasopkdopaskd.vue
+++ src/pages/opaksdopkasopkdopaskd.vue
@@ -1,7 +1,7 @@
 <template>
 
-  <v-img src="/src/assets/icon-email.png" alt="Logo" width="25" class="mb-3" contain></v-img>
-  <v-img src="/src/assets/icon-phone.png" alt="Logo" width="25" class="mb-3" contain></v-img>
+  <v-img src="/src/assets/icon-email2.png" alt="Logo" width="25" class="mb-3" contain></v-img>
+  <v-img src="/src/assets/icon-phone2.png" alt="Logo" width="25" class="mb-3" contain></v-img>
   <v-img src="/src/assets/icon-linkedin.png" alt="Logo" width="25" class="mb-3" contain></v-img>
   <v-img src="/src/assets/icon-instagram.png" alt="Logo" width="25" class="mb-3" contain></v-img>
   <v-img src="/src/assets/icon-youtube.png" alt="Logo" width="25" class="mb-3" contain></v-img>
